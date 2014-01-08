b343abd6c7312cb829d2d1667acc5e97bd5c4038
Sean Feil <sean@com.tophatandmonocle>
2014-01-07 22:10:50 -0700

    Adding network code

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 var io = require('socket.io').listen(8000);
+var fs = require('fs');
+
+eval(fs.readFileSync('json2.js') + '');
 
 io.sockets.on('connection', function (socket) {
-  socket.on('message', function (data) {
-    socket.emit('news', { hello: 'world' });
+  socket.on('greet', function (greeting) {
+      socket.broadcast.emit('join', socket.id);
+      socket.emit('greet', 'hello');
+      var connectedclients = [];
+      io.sockets.clients().forEach(function (client) {
+      	if(socket.id != client.id)
+      		connectedclients.push(client.id);
+      });
+      socket.emit('connected clients', JSON.stringify(connectedclients));
   });
 
-  socket.on('another-message', function (data) {
-    socket.emit('not-news', { hello: 'world' });
+  socket.on('keydown', function (data) {
+      console.log('keydown event ' + socket.id + " message: " + data);
+      socket.broadcast.emit('keydown', {client: socket.id, key: data});
+  });
+
+  socket.on('keydown', function (data) {
+      console.log('keydown event ' + socket.id + " message: " + data);
+      socket.broadcast.emit('keyup', {client: socket.id, key: data});
   });
 });
